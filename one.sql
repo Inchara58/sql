bf806f0cdfd12905fe84109228778cbedc3211af
Inchara58 <incharadr58@gmail.com>
2026-02-17 22:59:58 +0530

    sql beginner

---
+++ one.sql
@@ -0,0 +1,58 @@
+
+SELECT *
+FROM employee_salary
+WHERE salary > 50000;
+
+SELECT *
+FROM employee_salary
+WHERE salary >= 50000;
+
+SELECT *
+FROM employee_demographics
+WHERE gender = 'Female';
+
+
+#We can also return rows that do have not "Scranton"
+SELECT *
+FROM employee_demographics
+WHERE gender != 'Female';
+
+
+#We can use WHERE clause with date value also
+SELECT *
+FROM employee_demographics
+WHERE birth_date > '1985-01-01';
+
+
+
+
+# LIKE STATEMENT
+
+-- two special characters a % and a _
+
+-- % means anything
+SELECT *
+FROM employee_demographics
+WHERE first_name LIKE 'a%';
+
+-- _ means a specific value
+SELECT *
+FROM employee_demographics
+WHERE first_name LIKE 'a__';
+
+
+SELECT *
+FROM employee_demographics
+WHERE first_name LIKE 'a___%';
+
+
+
+
+
+
+
+
+
+
+
+
