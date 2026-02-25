dd43ea76b0f58dd984dd5b0262aaf31744ae60a1
Inchara58 <incharadr58@gmail.com>
2026-02-25 23:06:08 +0530

    practice

--- day3.sql
+++ day3.sql
@@ -47,3 +47,29 @@ select c.name,c.city,o.food_item
 from customers c
 inner join orders o
 on c.customer_id=o.customer_id;
+
+select c.name, o.oders,o.amount
+from customers c
+left join orders o
+on c.customer_id=o.customer_id;
+
+select c.name,c.city
+from customers c
+left join orders o
+on c.customer_id=o.customer_id
+where o.order_id is null;
+select * from orders
+
+select c.name,sum(o.amount)as total
+from customers c
+inner join orders o
+on c.customer_id=o.customer_id
+group by c.name
+having total>300;
+
+select c.name,c.city,o.amount
+from customers c
+inner join orders o
+on c.customer_id=o.customer_id
+where c.city='Banglore'
+order by o.amount;
