a5dd096f5158a1638b0019c8bf2a552d73570eab
Inchara58 <incharadr58@gmail.com>
2026-02-26 22:51:29 +0530

    new

---
+++ NES.sql
@@ -0,0 +1,20 @@
+select c.name,c.city
+from customers c
+left join orders o
+on c.customer_id=o.customer_id
+where o.order_id is null;
+select * from orders
+
+select c.name,sum(o.amount)as total
+from customers c
+inner join orders o
+on c.customer_id=o.customer_id
+group by c.name
+having total>300;
+
+select c.name,c.city,o.amount
+from customers c
+inner join orders o
+on c.customer_id=o.customer_id
+where c.city='Banglore'
+order by o.amount;
