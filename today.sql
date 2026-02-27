a90993f412ba870ccff37f8414469af80d4b466b
Inchara58 <incharadr58@gmail.com>
2026-02-27 23:35:09 +0530

    prac

--- today.sql
+++ today.sql
@@ -10,3 +10,24 @@ WHERE salary >= 50000;
 SELECT *
 FROM employee_demographics
 WHERE gender = 'Female';
+
+CREATE DATABASE zomato_db;
+USE zomato_db;
+
+CREATE TABLE zomato(
+    restaurant_id INT PRIMARY KEY,
+	name VARCHAR(10),
+    city VARCHAR(10),
+    Rating decimal,
+    opening_date DATE
+	
+
+);
+
+CREATE TABLE student(
+      student_id INT PRIMARY KEY,
+      name VARCHAR(20),
+      branch VARCHAR(10),
+      cgpa DECIMAL,
+      city VARCHAR(100)
+      );
