495954b7d00731906c24baa24c30c2eb261aa3c1
Inchara58 <incharadr58@gmail.com>
2026-03-03 23:01:07 +0530

    practice

--- newww.sql
+++ newww.sql
@@ -5,3 +5,12 @@ SELECT AVG(rating) FROM zomato;
 
 SELECT * FROM zomato
 WHERE rating > 4.28;
+
+SELECT * FROM zomato
+WHERE rating > (SELECT AVG(rating) FROM zomato);
+
+SELECT c.name, o.amount
+FROM customers c
+INNER JOIN orders o
+ON c.customer_id = o.customer_id
+WHERE o.amount > (SELECT AVG(amount) FROM orders);
