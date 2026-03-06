2096e7d93ea0ebbb015d72eb90d32e06033ed3b7
Inchara58 <incharadr58@gmail.com>
2026-03-06 23:49:35 +0530

    prac

---
+++ NEW.sql
@@ -0,0 +1,11 @@
+SELECT * FROM zomato
+WHERE rating > 4.28;
+
+SELECT * FROM zomato
+WHERE rating > (SELECT AVG(rating) FROM zomato);
+
+SELECT c.name, o.amount
+FROM customers c
+INNER JOIN orders o
+ON c.customer_id = o.customer_id
+WHERE o.amount > (SELECT AVG(amount) FROM orders);
