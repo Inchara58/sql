173fc72b4e8b3f67cec1ebdb1b0ba7cc381b76d9
Inchara58 <incharadr58@gmail.com>
2026-03-06 23:50:05 +0530

    prac

--- NEW.sql
+++ NEW.sql
@@ -9,3 +9,14 @@ FROM customers c
 INNER JOIN orders o
 ON c.customer_id = o.customer_id
 WHERE o.amount > (SELECT AVG(amount) FROM orders);
+
+
+select city from zomato 
+where rating between 4.0 and 4.5;
+
+select * from zomato
+where opening_date>'2008-01-01';
+
+select city,count(*),avg(rating) as avgg,max(rating)from zomato
+where city>0 and avgg>4.0
+order by avgg desc;
